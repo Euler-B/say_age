9f14a00e92d3fe35f4bed3f3fc4607c3621924fe
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-01 07:31:43 -0400

    first implementation

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hola Mundo, y bella ciudad de Merida");
+  int anio_de_nacimiento;
+  int edad;
+  const int anio_actual = 2022;
+
+  printf("Hola. Buen dia");
+  printf("Introduzca su año de nacimiento:\n");
+  scanf("%d", &anio_de_nacimiento);
+  edad = anio_actual - anio_de_nacimiento;
+  printf("Su edad es: %d  años \n", edad);
   return 0;
 }
