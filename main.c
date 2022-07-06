d3894024f3da3ba774ec74cc77e00c66f8351a19
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-06 13:14:21 -0400

    improvements aggregates

--- main.c
+++ main.c
@@ -7,24 +7,51 @@ int main() {
   const int anio_actual = 2022;
   const short  mes_actual = 7;
   const short dia_actual = 6;
-
+  // Step 1: show greetings
   printf("\t\t\t\t-----------------------------------------\n");
   printf("\t\t\t\t|                                       |\n");
   printf("\t\t\t\t|            Saludos                    |\n");
   printf("\t\t\t\t|                                       |\n");
   printf("\t\t\t\t-----------------------------------------\n");
-  printf("****** Introduzca su fecha de nacimiento en el formato: DD/MM/AA ********");
+
+  //Step 0: entrada  de datos 
+  printf("\t\t****** Introduzca su fecha de nacimiento en el formato: DD/MM/AA ******\n");
  
-  printf("\n--->: ");
+  printf("\n\t\t\t\t--->: ");
   scanf("%d", &dia_de_nacimiento);
-
-  printf("\n--->: ");
+  printf("\n\t\t\t\t--->: ");
   scanf("%d", &mes_de_nacimiento);
-
-  printf("\n--->: ");
+  printf("\n\t\t\t\t--->: ");
   scanf("%d", &anio_de_nacimiento);
 
-  edad = anio_actual - anio_de_nacimiento;
-  printf("Su edad es: %d  años \n", edad);
+  // Step 1 Procesado de datos
+
+  short anio_valido = anio_de_nacimiento <= anio_actual && anio_de_nacimiento >= 1903;
+  short mes_valido  = mes_de_nacimiento  <= 12          && mes_de_nacimiento  >= 1;
+  short dia_valido  = dia_de_nacimiento  <= 31          && mes_de_nacimiento  >= 1;
+
+  if (anio_valido && mes_valido && dia_valido){
+    edad = anio_actual - anio_de_nacimiento;
+
+    if (mes_de_nacimiento > mes_actual)
+      edad--;
+    if (mes_de_nacimiento == mes_actual && dia_de_nacimiento < dia_actual)
+      edad--;
+    
+    printf("\n");
+    printf("\t\t\t\t-----------------------------------------\n");
+    printf("\t\t\t\t|                                       |\n");
+    printf("\t\t\t\t|        su edad es: %d años            |\n", edad);
+    printf("\t\t\t\t|                                       |\n");
+    printf("\t\t\t\t-----------------------------------------\n");
+  } 
+  else {
+    printf("\n");
+    printf("\t\t\t\t-----------------------------------------\n");
+    printf("\t\t\t\t|                                       |\n");
+    printf("\t\t\t\t|   Por favor introduzca datos validos  |\n", edad);
+    printf("\t\t\t\t|                                       |\n");
+    printf("\t\t\t\t-----------------------------------------\n");
+  }
   return 0;
 }
